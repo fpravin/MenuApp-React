12f077bbc52da2f10e8830639b0129c8353f6d18
Pravin <pravinfernando3@gmail.com>
2022-10-31 00:04:37 +0530

    Lint Fix

--- src/components/layouts/MenuLayout.tsx
+++ src/components/layouts/MenuLayout.tsx
@@ -3,16 +3,16 @@ import { Outlet } from "react-router-dom";
 function MenuLayout() {
   return (
     <>
-        <header>
-            <h1>MENU</h1>
-            <h2>MENU</h2>
-            <h3>MENU</h3>
-            <h4>MENU</h4>
-            <h5>MENU</h5>
-            <h6>MENU</h6>
-            <p>MENU</p>
-        </header>
-        <Outlet />
+      <header>
+        <h1>MENU</h1>
+        <h2>MENU</h2>
+        <h3>MENU</h3>
+        <h4>MENU</h4>
+        <h5>MENU</h5>
+        <h6>MENU</h6>
+        <p>MENU</p>
+      </header>
+      <Outlet />
     </>
   );
 }
